61daee58f281e4eeb28b1220c3760e5ec9e09a02
sangram-automation <sangram.r121@gmail.com>
2019-02-10 13:37:22 +0530

    updated with two sop

--- Desktop/Selenium_Learning_Jenkins/src/browser/Demo.java
+++ Desktop/Selenium_Learning_Jenkins/src/browser/Demo.java
@@ -3,7 +3,7 @@ package browser;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello jenkinsssssssssss");
+		System.out.println("Hello jenkins push by Demo class");
 
 	}     
 
